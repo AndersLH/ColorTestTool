bd55757af50d68b27d6d0331472012f8b4fb2e09
AndersLH <andelha@stud.ntnu.no>
2024-10-26 22:00:16 +0200

    Initiated a React project

---
+++ src/Color.js
@@ -0,0 +1,13 @@
+import './Color.css';
+
+function Color() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+      </header>
+    </div>
+  );
+}
+
+export default Color;
